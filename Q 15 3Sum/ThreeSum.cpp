82e6bceafdbb5a0b2eb0e98cbf8fd096a34f45c9
DLohmann <dlohmann@ucmerced.edu>
2019-06-28 02:15:48 -0700

    Finished the algorithm. Code works, but is not fast enough for submission

--- Q 15 3Sum/ThreeSum.cpp
+++ Q 15 3Sum/ThreeSum.cpp
@@ -1,15 +1,43 @@
 class Solution {
 public:
+    static bool triplet_compare(vector<int> a, vector<int> b) {
+        // a < b
+        if (a[0] < b[0]) {
+            return true;
+        } else if (a[0] > b[0]) {
+            return false;
+        }
+        
+        if (a[1] < b[1]) {
+            return true;
+        } else if (a[1] > b[1]) {
+            return false;
+        }
+        
+        if (a[2] < b[2]) {
+            return true;
+        } else if (a[2] > b[2]) {
+            return false;
+        }
+        
+        return false;
+    }
     vector<vector<int>> threeSum(vector<int>& nums) {
+        if (nums.size() < 3) {
+            return  vector<vector<int>> {};
+        }
+        
+        sort(nums.begin(), nums.end());
+        
         // Sort the nums vector, and remove duplicates (so that none of the solution sets have duplicates):
-        //sort(nums.begin(), nums.end());
+        /*
         set<int> s;
         unsigned size = nums.size();
         for( unsigned i = 0; i < size; ++i ) {
             s.insert( nums[i] );
         }
         nums.assign( s.begin(), s.end() );
-        
+        */
         cout << "nums is: " << nums[0];
         for (int i = 1; i < nums.size(); i++) {
             cout << ", " << nums[i];
@@ -17,10 +45,15 @@ public:
         cout << endl;
         
         list< vector<int> > triplets;
+        int i, j, k;
         for(int i = 0  ; i < nums.size() - 2; i++) {
-        for(int j = i+1; i < nums.size() - 1; j++) {
-        for(int k = j+1; i < nums.size()    ; k++) {
-            if (nums[i] + nums[j] + nums[k] == 0) {
+        for(int j = i+1; j < nums.size() - 1; j++) {
+        for(int k = j+1; k < nums.size()    ; k++) {
+            if (nums[i] + nums[j] + nums[k] == 0 && // Check if it's 0
+               !binary_search(triplets.begin(), triplets.end(), vector<int>{nums[i], nums[j], nums[k]}, triplet_compare ) // Check if it's already in triplets
+               ) {
+                
+                
                 
                 vector<int> zero_triplet = {nums[i], nums[j], nums[k]};
                 //sort(triplet.begin(), triplet.end());
@@ -38,6 +71,9 @@ public:
         }
         }
         vector< vector<int> > sums {triplets.begin(), triplets.end()};
+        
+        // Remove duplicates
+        
         
         return sums;
     }
