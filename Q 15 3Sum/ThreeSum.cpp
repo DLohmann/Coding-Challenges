3f6b8864eb2996d770e1450511ee78e0a13e3e73
DLohmann <DLohmann@ucmerced.edu>
2019-12-27 03:24:46 -0800

    Replace spaces with tabs

--- Q 15 3Sum/ThreeSum.cpp
+++ Q 15 3Sum/ThreeSum.cpp
@@ -10,82 +10,81 @@
 
 class Solution {
 public:
-    static bool triplet_compare(std::vector<int> a, std::vector<int> b) {
-        // a < b
-        if (a[0] < b[0]) {
-            return true;
-        } else if (a[0] > b[0]) {
-            return false;
-        }
-        //hi
-        if (a[1] < b[1]) {
-            return true;
-        } else if (a[1] > b[1]) {
-            return false;
-        }
-        
-        if (a[2] < b[2]) {
-            return true;
-        } else if (a[2] > b[2]) {
-            return false;
-        }
-        
-        return false;
-    }
-    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
-        if (nums.size() < 3) {
-            return std::vector<std::vector<int>> {};
-        }
-        
-        sort(nums.begin(), nums.end());
-        
-        // Sort the nums std::vector, and remove duplicates (so that none of the solution sets have duplicates):
-        
-        std::set<int> s;
-        unsigned size = nums.size();
-        for(int num : nums) {
-            s.insert( num );
-        }
-        nums.assign( s.begin(), s.end() );
-        
-        
-        
-        std::list<std::vector<int> > triplets;
-        int i, j, k;    // Ensure i < j < k
-        for(int i = 0  ; i < nums.size() - 2; i++) {
-            for(int j = i+1; j < nums.size() - 1; j++) {
-            //for(int k = j+1; k < nums.size()    ; k++) {
-            k = lower_bound(nums.begin() + j + 1, nums.end(), -1*(nums[i] + nums[j])) - nums.begin();
-            
-            // Prevent k from going out of bounds
-            if (k == nums.size()){
-                k = 0;
-            }
-            if (nums[i] + nums[j] + nums[k] == 0 && // Check if it's 0
-                !binary_search(triplets.begin(), triplets.end(), std::vector<int>{nums[i], nums[j], nums[k]}, triplet_compare ) // Check if it's already in triplets
-            ) {
-                std::vector<int> zero_triplet = {nums[i], nums[j], nums[k]};
-                //sort(triplet.begin(), triplet.end());
-                std::cout << "[" << nums[i] << ", " << nums[j] << ", " << nums[k] << "]" << std::endl;
-                //std::cout << "[" << i << ", " << j << ", " << k << "]" << std::endl;
-                triplets.push_back(zero_triplet);
-                continue;
+	static bool triplet_compare(std::vector<int> a, std::vector<int> b) {
+		// a < b
+		if (a[0] < b[0]) {
+			return true;
+		} else if (a[0] > b[0]) {
+			return false;
+		}
+		//hi
+		if (a[1] < b[1]) {
+			return true;
+		} else if (a[1] > b[1]) {
+			return false;
+		}
+		
+		if (a[2] < b[2]) {
+			return true;
+		} else if (a[2] > b[2]) {
+			return false;
+		}
+		
+		return false;
+	}
+	std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+		if (nums.size() < 3) {
+			return std::vector<std::vector<int>> {};
+		}
+		
+		// Sort the nums std::vector, and remove duplicates (so that none of the solution sets have duplicates):
+		sort(nums.begin(), nums.end());
+		/*
+		std::set<int> s;
+		unsigned size = nums.size();
+		for(int num : nums) {
+			s.insert( num );
+		}
+		nums.assign( s.begin(), s.end() );
+		*/
+		
+		
+		std::list<std::vector<int> > triplets;
+		int i, j, k;	// Ensure i < j < k
+		for(int i = 0  ; i < nums.size() - 2; i++) {
+			for(int j = i+1; j < nums.size() - 1; j++) {
+			//for(int k = j+1; k < nums.size()	; k++) {
+			k = lower_bound(nums.begin() + j + 1, nums.end(), -1*(nums[i] + nums[j])) - nums.begin();
+			
+			// Prevent k from going out of bounds
+			if (k == nums.size()){
+				k = 0;
+			}
+			if (nums[i] + nums[j] + nums[k] == 0 && // Check if it's 0
+				!binary_search(triplets.begin(), triplets.end(), std::vector<int>{nums[i], nums[j], nums[k]}, triplet_compare ) // Check if it's already in triplets
+			) {
+				std::vector<int> zero_triplet = {nums[i], nums[j], nums[k]};
+				//sort(triplet.begin(), triplet.end());
+				std::cout << "[" << nums[i] << ", " << nums[j] << ", " << nums[k] << "]" << std::endl;
+				//std::cout << "[" << i << ", " << j << ", " << k << "]" << std::endl;
+				triplets.push_back(zero_triplet);
+				continue;
 
-            } else {
-                std::cout << "\t(" << nums[i] << ", " << nums[j] << ", " << nums[k] << ")" << std::endl;
-                //std::cout << "\t(" << i << ", " << j << ", " << k << ")" << std::endl;
+			} else {
+				std::cout << "\t(" << nums[i] << ", " << nums[j] << ", " << nums[k] << ")" << std::endl;
+				//std::cout << "\t(" << i << ", " << j << ", " << k << ")" << std::endl;
 
-            }
-            //}
-            }
-        }
-        std::vector< std::vector<int> > sums {triplets.begin(), triplets.end()};
-        
-        // Remove duplicates
-        
-        
-        return sums;
-    }
+			}
+			//}
+			}
+		}
+		std::vector< std::vector<int> > sums {triplets.begin(), triplets.end()};
+		
+		// Remove duplicates
+		
+		
+		return sums;
+	}
 };
 
 /*
