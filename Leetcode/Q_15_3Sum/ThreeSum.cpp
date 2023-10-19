7b71e04994388e1675ba61bb27a3bfefb0a79934
david <5475305+DLohmann@users.noreply.github.com>
2023-10-19 04:52:28 -0700

    Replaced tabs with spaces for Leetcode Q15

--- Leetcode/Q_15_3Sum/ThreeSum.cpp
+++ Leetcode/Q_15_3Sum/ThreeSum.cpp
@@ -12,119 +12,119 @@
 namespace three_sum {
 // Used to sort the triplets in a standard order
 bool Solution::triplet_compare(std::vector<int> a, std::vector<int> b) {
-	if (a[0] < b[0]) {
-		return true;
-	} else if (a[0] > b[0]) {
-		return false;
-	}
-	if (a[1] < b[1]) {
-		return true;
-	} else if (a[1] > b[1]) {
-		return false;
-	}
-	if (a[2] < b[2]) {
-		return true;
-	} else if (a[2] > b[2]) {
-		return false;
-	}
-	return false;
+    if (a[0] < b[0]) {
+        return true;
+    } else if (a[0] > b[0]) {
+        return false;
+    }
+    if (a[1] < b[1]) {
+        return true;
+    } else if (a[1] > b[1]) {
+        return false;
+    }
+    if (a[2] < b[2]) {
+        return true;
+    } else if (a[2] > b[2]) {
+        return false;
+    }
+    return false;
 }
 
 bool Solution::triplet_equals(std::vector<int> a, std::vector<int> b) {
-	return (a[0] == b[0] && a[1] == b[1] && a[2] == b[2] && a.size() == 3 && b.size() == 3);
+    return (a[0] == b[0] && a[1] == b[1] && a[2] == b[2] && a.size() == 3 && b.size() == 3);
 }
 
 std::vector<std::vector<int>> Solution::threeSum(std::vector<int>& input) {
-	if (input.size() < 3) {
-		return std::vector<std::vector<int>> {};
-	}
-	
-	// Sorts and removes duplicates (so that none of the solution sets have duplicates)
-	std::vector<int> nums(input);
-	sort(nums.begin(), nums.end());
-	// nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
-	std::list<std::vector<int> > triplets;	
-	for (unsigned int i = 0; i < nums.size() - 2; i++) {
-		for (unsigned int j = i + 1; j < nums.size() - 1; j++) {
-			for (unsigned int k = j + 1; k < nums.size(); k++) {
-				if (nums[i] + nums[j] + nums[k] == 0) {
-					triplets.push_back({nums[i], nums[j], nums[k]});
-				}
-				while (nums[k] == nums[k + 1]) {
-					k++;
-				}
-			}
-			while (nums[j] == nums[j + 1]) {
-				j++;
-			}
-		}
-		while (nums[i] == nums[i + 1]) {
-			i++;
-		}
-	}
-	
-	return std::vector< std::vector<int> >(triplets.begin(), triplets.end());
+    if (input.size() < 3) {
+        return std::vector<std::vector<int>> {};
+    }
+    
+    // Sorts and removes duplicates (so that none of the solution sets have duplicates)
+    std::vector<int> nums(input);
+    sort(nums.begin(), nums.end());
+    // nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
+    std::list<std::vector<int> > triplets;    
+    for (unsigned int i = 0; i < nums.size() - 2; i++) {
+        for (unsigned int j = i + 1; j < nums.size() - 1; j++) {
+            for (unsigned int k = j + 1; k < nums.size(); k++) {
+                if (nums[i] + nums[j] + nums[k] == 0) {
+                    triplets.push_back({nums[i], nums[j], nums[k]});
+                }
+                while (nums[k] == nums[k + 1]) {
+                    k++;
+                }
+            }
+            while (nums[j] == nums[j + 1]) {
+                j++;
+            }
+        }
+        while (nums[i] == nums[i + 1]) {
+            i++;
+        }
+    }
+    
+    return std::vector< std::vector<int> >(triplets.begin(), triplets.end());
 }
 
 std::vector<std::vector<int>> Solution::threeSumFaster(std::vector<int>& input) {
-	if (input.size() < 3) {
-		return std::vector<std::vector<int>> {};
-	}
-	// Sorts and removes duplicates (so that none of the solution sets have duplicates)
-	std::vector<int> nums(input);
-	sort(nums.begin(), nums.end());	
-	std::vector<std::vector<int> > triplets;	
-	for(unsigned int i = 0  ; i < nums.size() - 2; i++) {
-		for(unsigned int j = i+1; j < nums.size() - 1; j++) {
-			std::vector<int>::iterator it = lower_bound(nums.begin() + j + 1, nums.end(), -1*(nums[i] + nums[j]));
-			// Prevent k from going out of bounds
-			if (it == nums.end()){
-				continue;
-			}
-			unsigned int k = it - nums.begin();
-			if (nums[i] + nums[j] + nums[k] == 0) {
-				std::vector<int> zero_triplet = {nums[i], nums[j], nums[k]};
-				std::cout << "[" << nums[i] << ", " << nums[j] << ", " << nums[k] << "]" << std::endl;
-				triplets.push_back(zero_triplet);
-				continue;
-			} else {
-				std::cout << "\t(" << nums[i] << ", " << nums[j] << ", " << nums[k] << ")" << std::endl;
-			}
-			// TODO: Check for array index out of bounds
-			while (nums[j] == nums[j + 1] && j < nums.size()) {
-				j++;
-			}
-		}
-		while (nums[i] == nums[i + 1] && i < nums.size()) {
-			i++;
-		}
-	}
-	return triplets;
+    if (input.size() < 3) {
+        return std::vector<std::vector<int>> {};
+    }
+    // Sorts and removes duplicates (so that none of the solution sets have duplicates)
+    std::vector<int> nums(input);
+    sort(nums.begin(), nums.end());    
+    std::vector<std::vector<int> > triplets;    
+    for(unsigned int i = 0  ; i < nums.size() - 2; i++) {
+        for(unsigned int j = i+1; j < nums.size() - 1; j++) {
+            std::vector<int>::iterator it = lower_bound(nums.begin() + j + 1, nums.end(), -1*(nums[i] + nums[j]));
+            // Prevent k from going out of bounds
+            if (it == nums.end()){
+                continue;
+            }
+            unsigned int k = it - nums.begin();
+            if (nums[i] + nums[j] + nums[k] == 0) {
+                std::vector<int> zero_triplet = {nums[i], nums[j], nums[k]};
+                std::cout << "[" << nums[i] << ", " << nums[j] << ", " << nums[k] << "]" << std::endl;
+                triplets.push_back(zero_triplet);
+                continue;
+            } else {
+                std::cout << "\t(" << nums[i] << ", " << nums[j] << ", " << nums[k] << ")" << std::endl;
+            }
+            // TODO: Check for array index out of bounds
+            while (nums[j] == nums[j + 1] && j < nums.size()) {
+                j++;
+            }
+        }
+        while (nums[i] == nums[i + 1] && i < nums.size()) {
+            i++;
+        }
+    }
+    return triplets;
 }
 
 void printArr(std::vector<int> arr) {
-	std::cout << "[";
-	for (int i : arr) {
-		std::cout << " " << i;
-	}
-	std::cout << " ]" << std::endl;
+    std::cout << "[";
+    for (int i : arr) {
+        std::cout << " " << i;
+    }
+    std::cout << " ]" << std::endl;
 }
 
 std::vector<std::vector<int>> Solution::threeSumFastest(std::vector<int>& input) {
-	std::cout << "threeSumFastest" << std::endl;
-	printArr(input);
-	
-	if (input.size() < 3) {
-		return std::vector<std::vector<int>> {};
-	}
-	
-	std::vector<int> nums(input);
-	sort(nums.begin(), nums.end());
-	std::cout << "Sorted: ";
-	printArr(nums);
-	// nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
-	std::list<std::vector<int> > triplets;
-	
+    std::cout << "threeSumFastest" << std::endl;
+    printArr(input);
+    
+    if (input.size() < 3) {
+        return std::vector<std::vector<int>> {};
+    }
+    
+    std::vector<int> nums(input);
+    sort(nums.begin(), nums.end());
+    std::cout << "Sorted: ";
+    printArr(nums);
+    // nums.erase(std::unique(nums.begin(), nums.end()), nums.end());
+    std::list<std::vector<int> > triplets;
+    
 
 
 
@@ -161,8 +161,8 @@ std::vector<std::vector<int>> Solution::threeSumFastest(std::vector<int>& input)
         }
 
 
-	
-	return std::vector< std::vector<int> >(triplets.begin(), triplets.end());
+    
+    return std::vector< std::vector<int> >(triplets.begin(), triplets.end());
 }
 
 } // namespace three_sum
--- Leetcode/Q_15_3Sum/ThreeSum_test.cpp
+++ Leetcode/Q_15_3Sum/ThreeSum_test.cpp
@@ -16,201 +16,201 @@ using ::testing::ElementsAreArray;
 
 // Test simple leetcode case
 void PrintMatrix (std::vector<std::vector<int>> input) {
-	std::cout << std::endl;
-	for(std::vector<int> row : input) {
-		std::cout << "[";
-		for (int entry : row) {
-			std::cout << " " << entry;
-		}
-		std::cout << " ]" << std::endl;
-	}
-	std::cout << std::endl;
+    std::cout << std::endl;
+    for(std::vector<int> row : input) {
+        std::cout << "[";
+        for (int entry : row) {
+            std::cout << " " << entry;
+        }
+        std::cout << " ]" << std::endl;
+    }
+    std::cout << std::endl;
 }
 
 
 TEST(ThreeSum, Example1) {
-	std::vector<int> input = {-1, 0, 1, 2, -1, -4}; // sorted: -4, -1, -1, 0, 1, 2
-	Solution solution = Solution();
-	std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
-	std::vector<std::vector<int>> expected =
-		{
-			{-1, 0, 1},
-			{-1, -1, 2}
-		};
-	std::cout << "Expected output matrix:";
-	PrintMatrix(expected);
-	
-	std::cout << "Actual output matrix:";
-	PrintMatrix(actual);
-	
-	sort(actual.begin(), actual.end(), Solution::triplet_compare);
-	sort(expected.begin(), expected.end(), Solution::triplet_compare);
-	
-	EXPECT_EQ(actual.size(), expected.size());
-	for (unsigned int i = 0; i < expected.size(); i++) {
-		EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
-	}
+    std::vector<int> input = {-1, 0, 1, 2, -1, -4}; // sorted: -4, -1, -1, 0, 1, 2
+    Solution solution = Solution();
+    std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
+    std::vector<std::vector<int>> expected =
+        {
+            {-1, 0, 1},
+            {-1, -1, 2}
+        };
+    std::cout << "Expected output matrix:";
+    PrintMatrix(expected);
+    
+    std::cout << "Actual output matrix:";
+    PrintMatrix(actual);
+    
+    sort(actual.begin(), actual.end(), Solution::triplet_compare);
+    sort(expected.begin(), expected.end(), Solution::triplet_compare);
+    
+    EXPECT_EQ(actual.size(), expected.size());
+    for (unsigned int i = 0; i < expected.size(); i++) {
+        EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
+    }
 }
 
 TEST(ThreeSum, Example2) {
-	std::vector<int> input = {0, 1, 1};
-	Solution solution = Solution();
-	std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
-	std::vector<std::vector<int>> expected = {};
-	std::cout << "Expected output matrix:";
-	PrintMatrix(expected);
-	
-	std::cout << "Actual output matrix:";
-	PrintMatrix(actual);
-	
-	sort(actual.begin(), actual.end(), Solution::triplet_compare);
-	sort(expected.begin(), expected.end(), Solution::triplet_compare);
-	
-	EXPECT_EQ(actual.size(), expected.size());
-	for (unsigned int i = 0; i < expected.size(); i++) {
-		EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
-	}
+    std::vector<int> input = {0, 1, 1};
+    Solution solution = Solution();
+    std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
+    std::vector<std::vector<int>> expected = {};
+    std::cout << "Expected output matrix:";
+    PrintMatrix(expected);
+    
+    std::cout << "Actual output matrix:";
+    PrintMatrix(actual);
+    
+    sort(actual.begin(), actual.end(), Solution::triplet_compare);
+    sort(expected.begin(), expected.end(), Solution::triplet_compare);
+    
+    EXPECT_EQ(actual.size(), expected.size());
+    for (unsigned int i = 0; i < expected.size(); i++) {
+        EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
+    }
 }
 
 TEST(ThreeSum, Example3) {
-	std::vector<int> input = {0, 0, 0};
-	Solution solution = Solution();
-	std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
-	std::vector<std::vector<int>> expected = {{0, 0, 0}};
-	std::cout << "Expected output matrix:";
-	PrintMatrix(expected);
-	
-	std::cout << "Actual output matrix:";
-	PrintMatrix(actual);
-	
-	sort(actual.begin(), actual.end(), Solution::triplet_compare);
-	sort(expected.begin(), expected.end(), Solution::triplet_compare);
-	
-	EXPECT_EQ(actual.size(), expected.size());
-	for (unsigned int i = 0; i < expected.size(); i++) {
-		EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
-	}
+    std::vector<int> input = {0, 0, 0};
+    Solution solution = Solution();
+    std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
+    std::vector<std::vector<int>> expected = {{0, 0, 0}};
+    std::cout << "Expected output matrix:";
+    PrintMatrix(expected);
+    
+    std::cout << "Actual output matrix:";
+    PrintMatrix(actual);
+    
+    sort(actual.begin(), actual.end(), Solution::triplet_compare);
+    sort(expected.begin(), expected.end(), Solution::triplet_compare);
+    
+    EXPECT_EQ(actual.size(), expected.size());
+    for (unsigned int i = 0; i < expected.size(); i++) {
+        EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
+    }
 }
 
 TEST(ThreeSum, ZeroOneOne) {
-	std::vector<int> input = {0, 1, 1};
-	Solution solution = Solution();
-	std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
-	std::vector<std::vector<int>> expected = {};
-	std::cout << "Expected output matrix:";
-	PrintMatrix(expected);
-	
-	std::cout << "Actual output matrix:";
-	PrintMatrix(actual);
-	
-	sort(actual.begin(), actual.end(), Solution::triplet_compare);
-	sort(expected.begin(), expected.end(), Solution::triplet_compare);
-	
-	EXPECT_EQ(actual.size(), expected.size());
-	for (unsigned int i = 0; i < expected.size(); i++) {
-		EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
-	}
+    std::vector<int> input = {0, 1, 1};
+    Solution solution = Solution();
+    std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
+    std::vector<std::vector<int>> expected = {};
+    std::cout << "Expected output matrix:";
+    PrintMatrix(expected);
+    
+    std::cout << "Actual output matrix:";
+    PrintMatrix(actual);
+    
+    sort(actual.begin(), actual.end(), Solution::triplet_compare);
+    sort(expected.begin(), expected.end(), Solution::triplet_compare);
+    
+    EXPECT_EQ(actual.size(), expected.size());
+    for (unsigned int i = 0; i < expected.size(); i++) {
+        EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
+    }
 }
-	
+    
 TEST(ThreeSum, Example1ManyDuplicates) {
-	std::vector<int> input = {-1, 0, 1, -1, 2, 0, -1, 4, 4, 4, 4, -4};
-	// sorted: -4, -1, -1, -1, 0, 0, 1, 2, 4, 4, 4, 4
-	Solution solution = Solution();
-	std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
-	std::vector<std::vector<int>> expected =
-		{
-			{-4, 0, 4},
-			{-1, 0, 1},
-			{-1, -1, 2}
-		};
-	std::cout << "Expected output matrix:";
-	PrintMatrix(expected);
-	
-	std::cout << "Actual output matrix:";
-	PrintMatrix(actual);
-	
-	sort(actual.begin(), actual.end(), Solution::triplet_compare);
-	sort(expected.begin(), expected.end(), Solution::triplet_compare);
-	
-	EXPECT_EQ(actual.size(), expected.size());
-	for (unsigned int i = 0; i < expected.size(); i++) {
-		EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
-	}
+    std::vector<int> input = {-1, 0, 1, -1, 2, 0, -1, 4, 4, 4, 4, -4};
+    // sorted: -4, -1, -1, -1, 0, 0, 1, 2, 4, 4, 4, 4
+    Solution solution = Solution();
+    std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
+    std::vector<std::vector<int>> expected =
+        {
+            {-4, 0, 4},
+            {-1, 0, 1},
+            {-1, -1, 2}
+        };
+    std::cout << "Expected output matrix:";
+    PrintMatrix(expected);
+    
+    std::cout << "Actual output matrix:";
+    PrintMatrix(actual);
+    
+    sort(actual.begin(), actual.end(), Solution::triplet_compare);
+    sort(expected.begin(), expected.end(), Solution::triplet_compare);
+    
+    EXPECT_EQ(actual.size(), expected.size());
+    for (unsigned int i = 0; i < expected.size(); i++) {
+        EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
+    }
 }
 
 TEST(ThreeSum, WeightedNegative) {
-	std::vector<int> input = {-3, -2, 0, 0, 2};
-	Solution solution = Solution();
-	std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
-	std::vector<std::vector<int>> expected =
-		{
-			{-2, 0, 2}
-		};
-	std::cout << "Expected output matrix:";
-	PrintMatrix(expected);
-	
-	std::cout << "Actual output matrix:";
-	PrintMatrix(actual);
-	
-	sort(actual.begin(), actual.end(), Solution::triplet_compare);
-	sort(expected.begin(), expected.end(), Solution::triplet_compare);
-	
-	EXPECT_EQ(actual.size(), expected.size());
-	for (unsigned int i = 0; i < expected.size(); i++) {
-		EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
-	}
+    std::vector<int> input = {-3, -2, 0, 0, 2};
+    Solution solution = Solution();
+    std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
+    std::vector<std::vector<int>> expected =
+        {
+            {-2, 0, 2}
+        };
+    std::cout << "Expected output matrix:";
+    PrintMatrix(expected);
+    
+    std::cout << "Actual output matrix:";
+    PrintMatrix(actual);
+    
+    sort(actual.begin(), actual.end(), Solution::triplet_compare);
+    sort(expected.begin(), expected.end(), Solution::triplet_compare);
+    
+    EXPECT_EQ(actual.size(), expected.size());
+    for (unsigned int i = 0; i < expected.size(); i++) {
+        EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
+    }
 }
 
 TEST(ThreeSum, WeightedPositive) {
-	std::vector<int> input = {-2, 0, 0, 2, 3};
-	Solution solution = Solution();
-	std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
-	std::vector<std::vector<int>> expected =
-		{
-			{-2, 0, 2}
-		};
-	std::cout << "Expected output matrix:";
-	PrintMatrix(expected);
-	
-	std::cout << "Actual output matrix:";
-	PrintMatrix(actual);
-	
-	sort(actual.begin(), actual.end(), Solution::triplet_compare);
-	sort(expected.begin(), expected.end(), Solution::triplet_compare);
-	
-	EXPECT_EQ(actual.size(), expected.size());
-	for (unsigned int i = 0; i < expected.size(); i++) {
-		EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
-	}
+    std::vector<int> input = {-2, 0, 0, 2, 3};
+    Solution solution = Solution();
+    std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
+    std::vector<std::vector<int>> expected =
+        {
+            {-2, 0, 2}
+        };
+    std::cout << "Expected output matrix:";
+    PrintMatrix(expected);
+    
+    std::cout << "Actual output matrix:";
+    PrintMatrix(actual);
+    
+    sort(actual.begin(), actual.end(), Solution::triplet_compare);
+    sort(expected.begin(), expected.end(), Solution::triplet_compare);
+    
+    EXPECT_EQ(actual.size(), expected.size());
+    for (unsigned int i = 0; i < expected.size(); i++) {
+        EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
+    }
 }
 
 TEST(ThreeSum, TestCornerCases) {
-	std::vector<int> input = {-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4};
-	Solution solution = Solution();
-	std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
-	std::vector<std::vector<int>> expected =
-		{
-			{-4,0,4},
-			{-4,1,3},
-			{-3,-1,4},
-			{-3,0,3},
-			{-3,1,2},
-			{-2,-1,3},
-			{-2,0,2},
-			{-1,-1,2},
-			{-1,0,1}
-		};
-	std::cout << "Expected output matrix:";
-	PrintMatrix(expected);
-	
-	std::cout << "Actual output matrix:";
-	PrintMatrix(actual);
-	
-	sort(actual.begin(), actual.end(), Solution::triplet_compare);
-	sort(expected.begin(), expected.end(), Solution::triplet_compare);
-	
-	EXPECT_EQ(actual.size(), expected.size());
-	for (unsigned int i = 0; i < expected.size(); i++) {
-		EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
-	}
+    std::vector<int> input = {-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4};
+    Solution solution = Solution();
+    std::vector<std::vector<int>> actual = solution.threeSumFastest(input);
+    std::vector<std::vector<int>> expected =
+        {
+            {-4,0,4},
+            {-4,1,3},
+            {-3,-1,4},
+            {-3,0,3},
+            {-3,1,2},
+            {-2,-1,3},
+            {-2,0,2},
+            {-1,-1,2},
+            {-1,0,1}
+        };
+    std::cout << "Expected output matrix:";
+    PrintMatrix(expected);
+    
+    std::cout << "Actual output matrix:";
+    PrintMatrix(actual);
+    
+    sort(actual.begin(), actual.end(), Solution::triplet_compare);
+    sort(expected.begin(), expected.end(), Solution::triplet_compare);
+    
+    EXPECT_EQ(actual.size(), expected.size());
+    for (unsigned int i = 0; i < expected.size(); i++) {
+        EXPECT_THAT(actual[i], ElementsAreArray(expected[i]));
+    }
 }
