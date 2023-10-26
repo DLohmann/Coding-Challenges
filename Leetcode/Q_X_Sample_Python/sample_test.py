dbbd9b518b6e4e63b680341d007921c8248c4934
david <5475305+DLohmann@users.noreply.github.com>
2023-10-26 00:46:17 -0700

    Fixed python bazel test (which was intentionally broken to test GitHub actions unit test)

--- Leetcode/Q_X_Sample_Python/sample_test.py
+++ Leetcode/Q_X_Sample_Python/sample_test.py
@@ -4,7 +4,7 @@ import sample
 class TestFizzbuzz(unittest.TestCase):
     '''Unit test for sample fizzbuzz function.'''
     def test_100(self):
-        self.assertEqual(sample.fizzbuzz(100), 'bezz')
+        self.assertEqual(sample.fizzbuzz(100), 'buzz')
 
 
 if __name__ == "__main__":
